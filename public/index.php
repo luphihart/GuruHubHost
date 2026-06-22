906c95a642bd2c23cd49dc8fbfbeef63c39ebd7d
luphihart <lupiyohartadi@gmail.com>
2026-06-22 15:51:43 +0700

    chore: make index.php paths dynamic to support cPanel and local dev

--- public/index.php
+++ public/index.php
@@ -6,15 +6,25 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../guruhubapp/storage/framework/maintenance.php')) {
+    require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../guruhubapp/vendor/autoload.php')) {
+    require __DIR__.'/../guruhubapp/vendor/autoload.php';
+} else {
+    require __DIR__.'/../vendor/autoload.php';
+}
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../guruhubapp/bootstrap/app.php')) {
+    $app = require_once __DIR__.'/../guruhubapp/bootstrap/app.php';
+} else {
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+}
 
 $app->handleRequest(Request::capture());
